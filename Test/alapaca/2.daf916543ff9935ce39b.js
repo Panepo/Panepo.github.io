77d2fc702bd0456fccf256d6a831b9536da352a9
Panepo <whisper.ko@gmail.com>
2018-03-28 15:21:54 +0800

    add test page
    add test page Alapaca

---
+++ Test/alapaca/2.daf916543ff9935ce39b.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{1432:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default=function(e){return{path:":projectname",getComponent:function(e,n){t.e(7).then(function(e){var u=t(1441).default;n(null,u)}.bind(null,t)).catch(t.oe)}}}}});
+//# sourceMappingURL=2.daf916543ff9935ce39b.js.map
